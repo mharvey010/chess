ac345007fb86106b3c8f76778d4b57b5c905743c
mharvey010 <mveyhar1@gmail.com>
2026-09-15 22:48:39 -0600

    added move calculator for bishop (phase0)

--- shared/src/main/java/chess/ChessPiece.java
+++ shared/src/main/java/chess/ChessPiece.java
@@ -1,5 +1,6 @@
 package chess;
 
+import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
 
@@ -30,6 +31,15 @@ public class ChessPiece {
         PAWN
     }
 
+    public class DirectionVectors {
+        public static final int[][] DIAGONALS = {{1, 1}, {1, -1}, {-1, 1}, {-1, -1}};
+        public static final int[][] STRAIGHTS = {{1, 0}, {-1, 0}, {0, 1}, {0, -1}};
+
+        public static final int[][] OMNIDIRECTIONAL = {{1,1}, {1,-1}, {-1,1}, {-1,-1}, {1,0}, {-1,0}, {0,1}, {0,-1}};
+
+        public static final int[][] KNIGHT = {{2,1}, {2,-1}, {-2,1}, {-2,-1}, {1,2}, {1,-2}, {-1,2}, {-1,-2}};
+    }
+
     /**
      * @return Which team this chess piece belongs to
      */
@@ -52,6 +62,41 @@ public class ChessPiece {
      * @return Collection of valid moves
      */
     public Collection<ChessMove> pieceMoves(ChessBoard board, ChessPosition myPosition) {
+        ChessPiece piece = board.getPiece(myPosition);
+        if (piece.getPieceType() == PieceType.BISHOP) {
+            return calculateBishopMoves(board, myPosition);
+        }
+
         return List.of();
+    }
+
+    private boolean isOnBoard(int row, int col) {
+        return row <= 8 && row >0 && col <= 8 && col >0;
+    }
+
+    private Collection<ChessMove> calculateBishopMoves(ChessBoard board, ChessPosition myPosition) {
+        Collection<ChessMove> moves = new ArrayList<>();
+
+        int startRow = myPosition.getRow();
+        int startCol = myPosition.getColumn();
+        for (int[] direction : DirectionVectors.DIAGONALS) {
+            int row = startRow + direction[0];
+            int col = startCol + direction[1];
+
+            while (isOnBoard(row, col)) {
+                ChessPiece pieceAt = board.getPiece(new ChessPosition(row, col));
+                if (pieceAt == null) {
+                    moves.add(new ChessMove(myPosition, new ChessPosition(row, col), null));
+                } else {
+                    if (pieceAt.getTeamColor() != pieceColor) {
+                        moves.add(new ChessMove(myPosition, new ChessPosition(row, col), null));
+                    }
+                    break;
+                }
+                row += direction[0];
+                col += direction[1];
+            }
+        }
+        return moves;
     }
 }
